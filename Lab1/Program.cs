b05d33a5ea6d6a1ddf0498549545b976b95674d1
nigrumdominum <98053149+nigrumdominum@users.noreply.github.com>
2024-12-27 22:03:12 +0300

    Add files via upload

---
+++ Lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Habalov Oleg Olegovich
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Func<Action<string>, int, int> lambda = Function;
+        Action<string> action = Method;
+
+        Console.WriteLine(lambda(action, 4)); // true
+        Console.ReadLine();
+        Console.WriteLine(lambda(action, 5)); // false
+        Console.ReadLine();
+        Console.WriteLine(lambda(action, 2)); // true
+        Console.ReadLine();
+    }
+    static void Method(string s)
+    {
+        Console.WriteLine(s); // Вывод строкового выражения
+    }
+
+    static int Function(Action<string> action, int num)
+    {
+        //Проверка на четность натурального числа
+        int res = num % 2 == 0 ? 2 : 1;
+        action(res.ToString()); // Вывод строкового выражения
+        return res; // Вывод остатка от деления на 2
+    }
+}
