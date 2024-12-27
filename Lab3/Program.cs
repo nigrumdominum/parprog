5aff95e4d918ed99058d2ef9dd7758d9f1efd817
nigrumdominum <98053149+nigrumdominum@users.noreply.github.com>
2024-12-27 22:05:22 +0300

    Add files via upload

---
+++ Lab3/Program.cs
@@ -0,0 +1,28 @@
+﻿//Habalov Oleg Olegovich
+internal class Program
+{
+    public delegate int TakesAWhileDelegate(int data, int ms);
+    static int TakesAWhile(int data, int ms)
+    {
+        Console.WriteLine("TakesAWhile запущен");
+        Thread.Sleep(ms);
+        Console.WriteLine("TakesAWhile завершен");
+        return ++data;
+    }
+    static void Main(string[] args)
+    {
+        TakesAWhileDelegate dl = TakesAWhile;
+        IAsyncResult ar = dl.BeginInvoke(1, 3000, null, null);
+        while (true)
+        {
+            Console.Write(" . ");
+            if (ar.AsyncWaitHandle.WaitOne(50, false))
+            {
+                Console.WriteLine("Можно извлечь результат сейчас");
+                break;
+            }
+        }
+        int result = dl.EndInvoke(ar);
+        Console.WriteLine("result: (0}", result);
+    }
+}
